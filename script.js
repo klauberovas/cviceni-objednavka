c82be900141f5f298fd02d5cc559cd654bf418f9
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-09-30 20:54:05 +0200

    Done

--- script.js
+++ script.js
@@ -3,19 +3,9 @@ document.title = 'Objednávka';
 const formElm = document.querySelector('form');
 const creditCardElm = document.querySelector('#platebni-karta');
 const selectElm = document.querySelector('select');
-
-const proccesOfRequest = (event) => {
-  formElm.innerHTML =
-    '<h1>Hotovo</h1><p>Objednávka odeslána ke zpracování.</p>';
-
-  console.log(creditCardElm.value);
-  console.log(selectElm.value);
-};
-
-formElm.addEventListener('submit', proccesOfRequest);
+const messageParagraph = document.querySelector('p');
 
 formElm.addEventListener('input', (event) => {
-  const messageParagraph = document.querySelector('p');
   const shortPassword = 16 - creditCardElm.value.length;
   const longPassword = creditCardElm.value.length - 16;
 
@@ -25,4 +15,13 @@ formElm.addEventListener('input', (event) => {
     return (messageParagraph.innerHTML = `Číslo karty je příliš dlouhé. Přebývá ${longPassword} číslic.`);
   }
   return (messageParagraph.textContent = '');
+});
+
+formElm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formElm.innerHTML =
+    '<h1>Hotovo</h1><p>Objednávka odeslána ke zpracování.</p>';
+  console.log(creditCardElm.value);
+  console.log(selectElm.value);
+  messageParagraph.textContent = '';
 });
